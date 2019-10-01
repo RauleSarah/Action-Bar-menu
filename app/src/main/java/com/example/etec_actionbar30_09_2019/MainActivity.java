2dbfe87a71d91e6848ca4d8f28f2d0effab1539c
Raul e Sarah <raulglitzk26@ehotmail.com>
2019-09-30 22:29:31 -0300

    Action Bar Menu

---
+++ app/src/main/java/com/example/etec_actionbar30_09_2019/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.etec_actionbar30_09_2019;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Next(View view) {
+        Intent intent = new Intent(this,SegundaActivity.class);
+        startActivity(intent);
+
+
+    }
+
+}
